1f6c750cd5c136ac82605bb5f82cb2ef4e165446
snz2010 <snz2010@mail.ru>
2022-05-01 13:08:32 +0500

    2 вариант

--- app.py
+++ app.py
@@ -9,8 +9,14 @@ app.config['JSON_AS_ASCII'] = False
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
-def query_builder(it, cmd, val):
-    result = map(lambda v: v.strip(), it)
+""" В ФП функции должны принимать входные данные, обрабатывать их и возвращать выходные данные, 
+при этом не должны иметь какого-либо внутреннего состояния, которое будет влиять на результат.
+Функция должна всегда возвращать одинаковый результат при одинаковых входных данных.
+Это — фундаментальная концепция ФП """
+
+
+def query_builder(iterable_var, cmd, val):
+    result = map(lambda v: v.strip(), iterable_var)
     if cmd == "filter":
         result = filter(lambda v, txt=val: txt in v, result)
     if cmd == "map":
@@ -27,20 +33,43 @@ def query_builder(it, cmd, val):
     return result
 
 
+def get_commands(query):
+    cmd = []
+    arg = []
+    query_items = query.split("|")
+    for item in query_items:
+        if item == 'unique':
+            cmd.append('unique')
+            arg.append('')
+        elif 'file_name' in item:
+            filename = item.split(":")[1]
+        else:
+            split_item = item.split(":")
+            print(split_item)
+            cmd.append(split_item[0])
+            arg.append(split_item[1])
+    ret_list = list(zip(cmd, arg))
+    return ret_list, filename
 
 
-@app.route("/perform_query", methods=["POST"])
+
+@app.route("/perform_query", methods=["POST", "GET"])
 def perform_query():
-    # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
-    try:
-        cmd1 = request.args["cmd1"]
-        cmd2 = request.args["cmd2"]
-        val1 = request.args["val1"]
-        val2 = request.args["val2"]
-        file_name = request.args["file_name"]
-    except KeyError:
-        raise BadRequest(description="не все параметры переданы корректно")
-
+    if request.method == "POST":
+        # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
+        try:
+            cmd1 = request.args["cmd1"]
+            cmd2 = request.args["cmd2"]
+            val1 = request.args["val1"]
+            val2 = request.args["val2"]
+            file_name = request.args["file_name"]
+        except KeyError:
+            raise BadRequest(description="не все параметры переданы корректно")
+    elif request.method == "GET":
+        query_str = request.args.get("query")
+        ret_list, file_name = get_commands(query_str)
+        (cmd1, val1) = ret_list[0]
+        (cmd2, val2) = ret_list[1]
 
     # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
     file_path = os.path.join(DATA_DIR, file_name)
@@ -56,6 +85,7 @@ def perform_query():
         print(content)
 
     return app.response_class(content, content_type="text/plain")
+
 
 if __name__ == '__main__':
     app.run()
